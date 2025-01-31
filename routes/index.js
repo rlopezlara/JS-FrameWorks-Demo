557dab1014116bf922357db496af9d80b6dfa124
Rodrigo Lopez <134024353+rlopezlara@users.noreply.github.com>
2025-01-30 21:46:37 -0500

    Updated

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/about", function(req, res,next ) {
+  //res.render() is used for rendering the view
+  //views are located in the views folder
+  //about is the name of the view
+  res.render("about", { title: "About" });
+});
+
 module.exports = router;
